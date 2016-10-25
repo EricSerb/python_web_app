8861b6fd44390eb4265e9e10a92b2eee48489e6a
spanishgum <aps10d@my.fsu.edu>
2016-10-25 14:26:18 -0400

    Modified data.handler.spatial_query() so that it yields fetched results from the edge endpoint in chunks. Limit and chunk size kwargs were added in order to facilitate large result sizes.
     Must keep in mind that the get_kd() tree builder function loads a kdtree with an entire data subset. In order to create a tree with all the data, we will need to load all results from the spatial_query generator before passing to get_kd.

--- samos_map/data.py
+++ samos_map/data.py
@@ -13,6 +13,7 @@ import sys
 import time
 import json
 import codecs
+from math import ceil
 
 if sys.version_info[0] > 2:
     import urllib.request as urllib
@@ -58,86 +59,83 @@ class handler(object):
         # self.endpoint = endpoint
         self.edge = edge
         self.edge_params = { 'itemsPerPage' : None, \
-            'startIndex' : None, \
-            'bbox' : None
-        }
-        # self.edge_tail = '?itemsPerPage={items}&startIndex={start}&bbox={box}'
-        
-        
-        # try:
-            # self.solr = solr.Solr(self.endpoint)
-        # except (KeyboardInterrupt, SystemExit) as e:
-            # raise e
-        # except Exception as e:
-            # sys.stderr.write('Error with Solr connection')
-            # raise e
-    
-    
-    # def _exec(self, qry):
-        # sys.stderr.write('qry: {}'.format(qry))
-        # self.solr.SearchHandler(self.solr, '/select')(qry)
-    
-    
+                             'startIndex' : None, \
+                             'bbox' : None
+                             }
+
+
     def load_query(self, items=None, start=None, box=None):
         '''
         Loads parameters from dictionary with query values
         and returns a url string that will be used to retrieve data.
         '''
-        self.edge_params['itemsPerPage'] = items
-        self.edge_params['startIndex'] = start
+        self.edge_params['itemsPerPage'] = 0
+        # self.edge_params['itemsPerPage'] = items
+        self.edge_params['startIndex'] = 0
+        # self.edge_params['startIndex'] = start
         self.edge_params['bbox'] = box
-        
+
         tail = '&'.join('{}={}'.format(k, self.edge_params[k]) \
-            for k in self.edge_params if self.edge_params[k] is not None)
-        
+                        for k in self.edge_params if self.edge_params[k] is not None)
+
         if tail:
             qry_str = '?'.join((self.edge, tail))
         else:
             qry_str = self.edge
-        
+
         return qry_str
-    
-    def spatial_query(self, lats, lons):
-        '''
-        Handles the execution of querying the solr endpoint with a 
-        bounding box representing lat/lon ranges.
-        '''
-        box = '{},{},{},{}'.format(lons[0], lats[0], lons[1], lats[1])
-        start = 0
-        items = 100
-        
-        qry_url = self.load_query(items=items, start=start, box=box)
-        
+
+
+    def exec_query(self, url):
         if sys.version_info[0] < 3:
-            response = urllib.urlopen(qry_url)
+            response = urllib.urlopen(url)
             data = json.loads(response.read())
             return data
         elif sys.version_info[0] > 2:
             reader = codecs.getreader('utf-8')
-            response = urllib.urlopen(qry_url)
+            response = urllib.urlopen(url)
             data = json.load(reader(response))
             return data
-        
+
+
+    def spatial_query(self, lats, lons, limit=5000, chunk=1000):
+        '''
+                Handles the execution of querying the solr endpoint with a
+                bounding box representing lat/lon ranges.
+                '''
+        box = '{},{},{},{}'.format(lons[0], lats[0], lons[1], lats[1])
+        start = 0
+        cnt = self.get_cnt(box)
+        print('Spatial query: {} results'.format(cnt))
+        n = ceil(cnt / chunk)
+        loops = min(n, ceil(limit / chunk)) if isinstance(limit, int) else n
+        for _ in range(loops):
+            url = self.load_query(items=chunk, start=start, box=box)
+            start += chunk
+            yield self.exec_query(url)
+
         ## BELOW IS DEPRACATED RIGHT NOW BUT LEFT FOR FUTURE REFERENCE
         # for x, y in zip(lats, lons):
-            # assert -180 < x <= 180 and -90 < y <= 90, "Spatial parameters out of bounds:" \
-                # " -180 < x <= 180 and -90 < y <= 90"
+        # assert -180 < x <= 180 and -90 < y <= 90, "Spatial parameters out of bounds:" \
+        # " -180 < x <= 180 and -90 < y <= 90"
         # bbox = [lats, lons].flatten()
         # qry = '{}/select?q=*:*&fq=bounding_box:[{},{} TO {},{}]'
         # return self._exec(qry.format(self.endpoint,
-            # lats[0], lats[1], lons[0], lons[1]))
-    
+        # lats[0], lats[1], lons[0], lons[1]))
+
+    def get_cnt(self, box):
+        url = self.load_query(items=0, start=0, box=box)
+        return self.exec_query(url)['results']
+
     def get_kd(self, response):
         '''
         Given an EDGE response, this will construct a KD tree 
         with that data.
         '''
         global scilibs_available
-        if not scilibs_available:
-            sys.stderr.write('Warning: data.handler.load_kd() ' \
-            'could not be ran because of missing dependency.\n')
-            return
-        # coords = np.
+        assert scilibs_available, 'Warning: data.handler.load_kd() ' \
+                             'could not be ran because of missing dependency.'
+
         n = response['itemsPerPage']
         arr = np.empty((n, 2), dtype=np.float32)
         for i, doc in enumerate(response['results']):
