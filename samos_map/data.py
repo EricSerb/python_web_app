ac7a7c66f391ceb2d1a08170d2b891243033eace
Adam Stallard <astallard@doms.coaps.fsu.edu>
2016-10-25 18:56:47 -0400

    Broke down a few functions. Used more generators. Removed shapely dependency, replaced wkt parser using regex. Added a namedtuple 'pack' used to represent a 'doc' result. Whichever document keys are passed to the handler determines what is extracted from edge.

--- samos_map/data.py
+++ samos_map/data.py
@@ -9,11 +9,13 @@ to populate a web map on their end.
 '''
 
 import os
+import re
 import sys
 import time
 import json
 import codecs
 from math import ceil
+from collections import Iterable, namedtuple as ntuple
 
 if sys.version_info[0] > 2:
     import urllib.request as urllib
@@ -24,19 +26,12 @@ try:
     scilibs_available = True
     import numpy as np
     from scipy.spatial import cKDTree
-    import shapely.wkt as wkt
 except ImportError:
     scilibs_available = False
     sys.stderr.write('Warning: Missing scientific libraries.\n' \
         'Please make sure scipy and shapely are installed.\n')
     pass
 
-## DEPRACTAED LIBS .. FOR NOW
-# import requests
-# import solr
-
-# SOLR_PORT = 8983
-# SOLR_ENDPOINT = 'http://localhost:{}/solr/samos'.format(SOLR_PORT)
 
 EDGE_ENDPOINT = 'http://doms.coaps.fsu.edu/ws/search/samos'
 
@@ -46,9 +41,7 @@ class handler(object):
     Simple handler for now. This will likely be broken down 
     into individual components as more code is added.
     '''
-    # global SOLR_ENDPOINT
-    # def __init__(self, endpoint=SOLR_ENDPOINT):
-    def __init__(self, edge=EDGE_ENDPOINT):
+    def __init__(self, edge=EDGE_ENDPOINT, keys=None):
         '''
         Loading 2 endpoints for now, because were not sure which one
         will be used.
@@ -58,9 +51,15 @@ class handler(object):
                              'startIndex' : None, \
                              'bbox' : None
                              }
+        self.wktreg = re.compile(r'(\d+(?:\.\d*)?)')
+        
+        self.datakeys = keys or ('point', 'time')
+        assert isinstance(self.datakeys, Iterable)
+        
+        self.pack = ntuple('pack', ' '.join(self.datakeys))
 
 
-    def load_query(self, items=None, start=None, box=None):
+    def config(self, items=None, start=None, box=None):
         '''
         Loads parameters from dictionary with query values
         and returns a url string that will be used to retrieve data.
@@ -80,7 +79,7 @@ class handler(object):
         return qry_str
 
 
-    def exec_query(self, url):
+    def query(self, url):
         if sys.version_info[0] < 3:
             response = urllib.urlopen(url)
             data = json.loads(response.read())
@@ -92,27 +91,46 @@ class handler(object):
             return data
 
 
-    def spatial_query(self, lats, lons, limit=5000, chunk=1000):
+    def loadpoint(self, s):
         '''
-                Handles the execution of querying the solr endpoint with a
-                bounding box representing lat/lon ranges.
-                '''
+        My own simple version of shaoply.wkt.loads() so we don't require
+        shapely libraries.
+        '''
+        assert isinstance(s, (str, unicode, bytearray))
+        assert s.lower().startswith('point(')
+        
+        nums = map(float, re.findall(self.wktreg, s))
+        return tuple(iter(nums))
+
+
+    def spatial(self, lats, lons, limit=5000, chunk=1000):
+        '''
+        Handles the execution of querying the solr endpoint with a
+        bounding box representing lat/lon ranges.
+        '''
+        assert isinstance(limit, int)
+        assert isinstance(chunk, int)
+        
         box = '{},{},{},{}'.format(lons[0], lats[0], lons[1], lats[1])
         start = 0
-        cnt = self.get_cnt(box)
-        print('Spatial query: {} results'.format(cnt))
+        cnt = self.count(box)
+        
         n = ceil(cnt / chunk)
-        loops = min(n, ceil(limit / chunk)) if isinstance(limit, int) else n
+        loops = int(min(n, ceil(limit / chunk)) if limit else n)
+        
         for _ in range(loops):
-            url = self.load_query(items=chunk, start=start, box=box)
+            url = self.config(items=chunk, start=start, box=box)
             start += chunk
-            yield self.exec_query(url)
+            yield self.query(url)
 
-    def get_cnt(self, box):
-        url = self.load_query(items=0, start=0, box=box)
-        return self.exec_query(url)['results']
 
-    def get_kd(self, response):
+    def count(self, box):
+        url = self.config(items=0, start=0, box=box)
+        page = self.query(url)
+        return page['totalResults']
+
+
+    def extract(self, responses):
         '''
         Given an EDGE response, this will construct a KD tree 
         with that data.
@@ -120,12 +138,16 @@ class handler(object):
         global scilibs_available
         assert scilibs_available, 'Warning: data.handler.load_kd() ' \
                              'could not be ran because of missing dependency.'
+        
+        for chunk in responses:
+            for res in chunk['results']:
+                yield self.pack(**{k : res[k] for k in self.datakeys})
 
-        n = response['itemsPerPage']
+    def kd(self, packs):
+        points, times = zip(*[(self.loadpoint(d.point), d.time) \
+            for d in packs])
+        n = len(points)
         arr = np.empty((n, 2), dtype=np.float32)
-        for i, doc in enumerate(response['results']):
-            p = wkt.loads(doc['point'])
-            arr[i,:] = p.x, p.y
+        for i, p in enumerate(points):
+            arr[i,:] = p[0], p[1]
         return cKDTree(arr)
-
-
--- samos_map/test.py
+++ samos_map/test.py
@@ -25,7 +25,7 @@ def test_qry_loader(show=False):
     handle = data.handler()
     
     # TEST 1
-    qs = handle.load_query(items=None, start=None, box=None)
+    qs = handle.config(items=None, start=None, box=None)
     assert qs == data.EDGE_ENDPOINT, 'Query construction failure: {}' \
             '\nExpected: {}'.format(qs, data.EDGE_ENDPOINT)
 
@@ -33,7 +33,7 @@ def test_qry_loader(show=False):
         pprint.pprint(qs)
     
     # TEST 2
-    qs = handle.load_query(items=10, start=10, box='-45,15,-30,30')
+    qs = handle.config(items=10, start=10, box='-45,15,-30,30')
     try:
         for sub in \
             'itemsPerPage=10&startIndex=10&bbox=-45,15,-30,30'.split('&'):
@@ -51,7 +51,7 @@ def test_qry_executor(show=False):
     handle = data.handler()
     lats = [15, 30]
     lons = [-45, -30]
-    res = handle.spatial_query(lats, lons)
+    res = handle.spatial(lats, lons)
     assert res is not None # Consider beefing this test assertion up
     if show:
         pprint.pprint(res)
@@ -61,7 +61,7 @@ def test_qry_executor(show=False):
 def test_extraction(show=False):
     handle = data.handler()
     res = test_qry_executor(show)
-    kd = handle.get_kd(res)
+    kd = handle.kd(handle.extract(res))
     if show and kd is not None:
         print(kd.data)
 
