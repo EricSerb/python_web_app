3db03b0912e9c897b563a4a71568a4348dded2c4
spanishgum <aps10d@my.fsu.edu>
2016-12-05 19:11:52 -0500

    added src file for ks marker cluster:

--- samos_map/static/js/index.js
+++ samos_map/static/js/index.js
@@ -36,19 +36,19 @@ function pinTheMap(data) {
 
     //add the new pins
 
-     var clusterArray = L.markerClusterGroup({
-        iconCreateFunction: function(cluster){
-            return L.divIcon({html: '<b>' + cluster.getChildCount() + '</b>'});
-        }
-    });
+     // var clusterArray = L.markerClusterGroup({
+        // iconCreateFunction: function(cluster){
+            // return L.divIcon({html: '<b>' + cluster.getChildCount() + '</b>'});
+        // }
+    // });
 
     var markerArray = new Array(data['points'].length);
     for (var i = 0; i < data['points'].length; i++) {
         point = data['points'][i];
         markerArray[i] = L.marker([point.lat, point.lon]).bindPopup(point.idx);
 
-        clusterArray.addLayer(markerArray[i]);
-        console.log(markerArray[i]);
+        // clusterArray.addLayer(markerArray[i]);
+        // console.log(markerArray[i]);
         markerArray[i].on('click',
             function(e, feature) {
                 this._popup.setContent(ancillaryData(this._popup));
@@ -56,16 +56,16 @@ function pinTheMap(data) {
 
     }
 
-    map.addLayer(clusterArray);
-    // markerLayerGroup = L.layerGroup(markerArray).addTo(map); 
+    // map.addLayer(clusterArray);
+    markerLayerGroup = L.layerGroup(markerArray).addTo(map); 
 
-    clusterArray.on('click', function(a) {
-        console.log('clusterArray click' + a.layer);
-    });
+    // clusterArray.on('click', function(a) {
+        // console.log('clusterArray click' + a.layer);
+    // });
 
-    clusterArray.on('clusterclick', function (a){
-        console.log('ckusterArray clusterclick' + a.layer.getAllChildMarkers().length);
-    });
+    // clusterArray.on('clusterclick', function (a){
+        // console.log('ckusterArray clusterclick' + a.layer.getAllChildMarkers().length);
+    // });
 }
 
 function ancillaryData(popup) {
