5e26c907fa386d741e5a3bcaf902100408fa1686
spanishgum <aps10d@my.fsu.edu>
2016-12-05 21:18:48 -0500

    testing bbox func

--- samos_map/kd.py
+++ samos_map/kd.py
@@ -123,7 +123,7 @@ class Container(object):
                     yield from flatten(i)
         
         qpnts = list(flatten([zip(_x, Y) for _x in permutations(X, len(Y))]))
-        res = list(set(flatten(self.tree.query(qpnts, k=k, distance_upper_bound=max_d)[1], lowest_inst=int)))
+        res = list(set(flatten(self.tree.query(qpnts, k=k, distance_upper_bound=max_d)[1], lowest_inst=np.int64)))
         
         # res = 
         
