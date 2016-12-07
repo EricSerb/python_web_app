d4a342eb8b301201cb9fee6bd1997ff5eec2ef82
EricSerb <ericserbousek8@gmail.com>
2016-12-06 20:00:25 -0500

    fixed testing code. Didn't need the init and made Container object a singleton

--- samos_map/tests.py
+++ samos_map/tests.py
@@ -5,30 +5,28 @@ import requests
 
 class Test(unittest.TestCase):
 
-    def __init__(self, *args, **kwargs):
-        self.con = kd.Container(limit=100)
-        super(self, Test).__init__(*args, **kwargs)
+    con = kd.Container(limit=100)
 
     def test_vars(self):
-        assert self.con.limit == 100
-        assert self.con.loadtime > 0
-        assert not isinstance(self.con.tree, None)
-        assert self.size > 0
-        assert self.total > 0
-        assert not isinstance(self.con.api, None)
-        assert not isinstance(self.con.data, None)
+        assert Test.con.limit == 100
+        assert Test.con.loadtime > 0
+        assert not isinstance(Test.con.tree, None)
+        assert Test.con.size > 0
+        assert Test.con.total > 0
+        assert not isinstance(Test.con.api, None)
+        assert not isinstance(Test.con.data, None)
 
     def test_nearest(self):
-        assert isinstance(self.con.nearest(0, 0), str)
+        assert isinstance(Test.con.nearest(0, 0), str)
 
     def test_data(self):
-        assert len(self.con.data['time']) == len(self.con.data['meta']) == \
-               len(self.con.data['loc'])
-        assert self.con.data['meta'][99]
-        assert not self.con.data['meta'][100]
+        assert len(Test.con.data['time']) == len(Test.con.data['meta']) == \
+               len(Test.con.data['loc'])
+        assert Test.con.data['meta'][99]
+        assert not Test.con.data['meta'][100]
 
     def test_anc(self):
-        anc = self.con.ancillary(self.con.data['meta'][99])
+        anc = Test.con.ancillary(Test.con.data['meta'][99])
         assert isinstance(anc, dict)
         for key in ['meta', 'time', 'loc', 'wind_u', 'wind_v', 'wind_speed',
                     'SSS', 'SST', 'thredds']:
@@ -37,18 +35,18 @@ class Test(unittest.TestCase):
         assert res.status_code == 200
 
     def test_convpoint(self):
-        lat_lon = self.con._convpoint('50.0440711975, -128.118637085')
+        lat_lon = Test.con._convpoint('50.0440711975, -128.118637085')
         assert isinstance(lat_lon, tuple)
         assert lat_lon[0] == -128.118637085
         assert lat_lon[1] == 50.0440711975
-        lat_lon = self.con._convpoint('-50.0440711975, -128.118637085')
+        lat_lon = Test.con._convpoint('-50.0440711975, -128.118637085')
         assert isinstance(lat_lon, tuple)
         assert lat_lon[0] == -128.118637085
         assert lat_lon[1] == -50.0440711975
 
     def test_bbox(self):
-        pnts = self.con.bbox((91, -91), (-181, 181))
-        assert len(pnts) == len(self.con.limit)
+        pnts = Test.con.bbox((91, -91), (-181, 181))
+        assert len(pnts) == len(Test.con.limit)
 
 
 if __name__ == "__main__":
