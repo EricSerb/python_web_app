5b177a8b83079a26567c71aa7c8dced66e5c1918
EricSerb <ericserbousek8@gmail.com>
2016-12-06 23:44:07 -0500

    stupid mistake

--- samos_map/tests.py
+++ samos_map/tests.py
@@ -85,7 +85,7 @@ class Test(unittest.TestCase):
     def test_add(self):
         idx_0 = {'meta': Test.con.data['meta'][0], 'time': Test.con.data[
             'time'][0], 'loc': Test.con.data['loc'][0][:]}
-        idx_0['loc'] = ','.join(reversed(idx_0['loc']))
+        idx_0['loc'] = ','.join(map(str, reversed(idx_0['loc'])))
         Test.con._add(idx_0, -1)
         for key in ['meta', 'time', 'loc']:
             assert Test.con.data[key][0] == Test.con.data[key][-1]
